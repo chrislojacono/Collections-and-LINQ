09cff3d19fbc494aa55d9121092e7077c820b152
Chris LoJacono <clojacono92@gmail.com>
2021-02-01 18:34:00 -0600

    inclass

---
+++ CollectionsAndLinq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsAndLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //anything with angle brackets after the type name is called a generic type
+            // List<T> Generic List
+            //Pronounced List of T
+            //T is a Generic Type Parameter
+            //String in this case closes the generic type----tells you what it's going to contain
+            var names = new List<string> { "Chris", "Ryan", "Joseph" };
+            names.Add("Jordan");
+
+
+        }
+    }
+}
